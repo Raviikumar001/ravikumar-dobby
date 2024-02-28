3a2be7f22e5ed5dd01f83ab1214e27980582ae12
Ravi <samcollins9899@gmail.com>
2024-02-28 20:36:48 +0530

    fixed text typo

--- frontend/src/MainApp/uploadPicture.tsx
+++ frontend/src/MainApp/uploadPicture.tsx
@@ -95,7 +95,7 @@ const UploadPicture: React.FC = () => {
             <Link to="/app">
               <ArrowLeft />{" "}
             </Link>
-            <h2> Create a New Task</h2>
+            <h2> Upload an new Image</h2>
             <p></p>
           </div>
           <form className="mt-10" onSubmit={(event) => submitForm(event)}>
@@ -104,7 +104,7 @@ const UploadPicture: React.FC = () => {
             w-full
             " />
             <input type="file" onChange={handleFileChange} className="block mt-3 w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400" aria-describedby="file_input_help" id="file_input" />
-            <p className="mt- text-sm text-gray-500 dark:text-gray-300" id="file_input_help">
+            <p className="mt-2 text-sm text-gray-800 dark:text-gray-300" id="file_input_help">
               SVG, PNG, JPG or GIF (MAX. 2MB).
             </p>
             {loading && <LoadingComponent message="Uploading" />}
